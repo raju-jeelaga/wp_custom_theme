e5bb18637277032704c6079c7182deb06a792914
Raju Jeelaga <rajujeelaga@gmail.com>
2022-08-08 15:27:52 +0530

    All format of events code is added
    All format of events code is added

--- front-page.php
+++ front-page.php
@@ -17,27 +17,46 @@
 
 
            <?php 
+           $today = date('Ymd');
           $homepageEvents = new WP_Query(array(
-            'posts_per_page' => 2,
-            'post_type' => 'our_events'
+            'posts_per_page' => 3,
+            'post_type' => 'our_events',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            )
           ));
-
+          if($homepageEvents->have_posts())
           while($homepageEvents->have_posts()) {
             $homepageEvents->the_post(); ?>
             <div class="event-summary">
               <a class="event-summary__date t-center" href="#">
-                <span class="event-summary__month">Mar</span>
-                <span class="event-summary__day">25</span>  
+                <span class="event-summary__month">
+                <?php 
+                	$eventDate = new DateTime(get_field('event_date'));
+                	echo $eventDate->format('M');
+                ?>
+                </span>
+                <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>  
               </a>
               <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
                 <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
               </div>
             </div>
-          <?php }
+          <?php } else {
+          	echo "No More Events";
+          }
         ?>
-
-          <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+        
+          <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('our_events') ?>" class="btn btn--blue">View All Events</a></p>
         </div>
       </div>
       <div class="full-width-split__two">
--- functions.php
+++ functions.php
@@ -5,11 +5,60 @@
  * hard-coded <title> tag in the document head, and expect WordPress to
  * provide it for us.
  */
-add_theme_support( 'title-tag' );
+function university_features() {
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
+
+add_theme_support( 'post-thumbnails' );
 
 function wp_custom_theme() {
     //wp_enqueue_style( 'style', get_template_directory_uri() );
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
   }
 add_action( 'wp_enqueue_scripts', 'wp_custom_theme' );
+
+
+// Register Custom post Type
+function university_post_types() {
+  register_post_type('our_events', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', "thumbnail", 'excerpt'),
+    'rewrite' => array('slug' => 'our_events'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => 'Add New Event',
+      'edit_item' => 'Edit Event',
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar'
+  ));
+}
+
+add_action('init', 'university_post_types');
+
+// Events Pagination
+function university_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            ));
+  }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
+
 
